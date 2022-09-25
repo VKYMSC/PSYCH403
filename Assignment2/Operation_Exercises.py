862a0716d1e0729c8cc9129dc2d0c1a6d9932be0
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-09-25 16:29:12 -0600

    Create Operation_Exercises.py

---
+++ Assignment2/Operation_Exercises.py
@@ -0,0 +1,22 @@
+## Operation Exercises
+## Question 1:
+print(5/2)
+print(5.0/2.0)
+
+## Question 2:
+print(1%2)
+print(3%5)
+print(7%3)
+
+##Question 3:
+print(1**3)
+print(2**3)
+print(3**4)
+print(1//3)
+print(2//3)
+print(6//2)
+
+##Question 4:
+print(1+ 2 + 3 * 4 / 5)
+print(3+ 5 + 7 * 6 / 2)
+print(12+ 23 + 16 * 8 /35 )
