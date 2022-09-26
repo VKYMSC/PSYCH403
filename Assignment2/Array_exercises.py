714dac3bdb760a435b2691d26aa2affba807e894
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-09-25 19:33:29 -0600

    Create Array_exercises.py

---
+++ Assignment2/Array_exercises.py
@@ -0,0 +1,10 @@
+##Array exercises
+import numpy as np
+mixnums = np.array([1,2.0,3,4.0,5,6.0])
+print(mixnums)
+mixtypes = np.array([1,2,3.0,4.0,"5","6"])
+print(mixtypes)
+oddarrary = np.arange(1,101,2)
+print(oddarrary)
+logarray = np.logspace(1,5,16)
+print(logarray)
