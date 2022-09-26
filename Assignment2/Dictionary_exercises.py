ec5ae763f5a21f47157743f4b3447a155c5a3007
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-09-25 19:05:56 -0600

    Create Dictionary_exercises.py

---
+++ Assignment2/Dictionary_exercises.py
@@ -0,0 +1,10 @@
+##Dictionary exercises
+about_me = {
+    'name': 'Ruihan',
+    'age': 22.0,
+    'year': 4,
+    'favourite foods': ['ice cream','mango','gummy candy','cotton candy']}
+
+print(about_me)
+print(type(about_me))
+print(len(about_me))
