87d197c12dd3bbd9ba1a41f68a06707116f3cfa4
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-09-25 18:28:45 -0600

    Create Boolean_exercises.py

---
+++ Assignment2/Boolean_exercises.py
@@ -0,0 +1,10 @@
+##Boolean exercises 
+print(1 == 1.0)
+print("1"=="1.0")
+print(5 == (3+2))
+#5 ways to get true
+print (1==1.0 and not "1"=="1.0" and 5 == (3+2))
+print (1==1.0 or  "1"=="1.0" and 5 == (3+2))
+print (1==1.0 or  "1"=="1.0" or 5 == (3+2))
+print (1==1.0 and not "1"=="1.0" or 5 == (3+2))
+print (1==1.0 and "1" =="1.0" or 5 == (3+2))
