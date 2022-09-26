177b71ae407c9716d058c0744400e7b7ebcb34ac
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-09-25 18:17:01 -0600

    Update yourname.py

--- Assignment2/yourname.py
+++ Assignment2/yourname.py
@@ -4,4 +4,30 @@ print("U")
 print("I")
 print("H")
 print("A")
-print("H")
+print("N")
+
+## Variable exercises
+letter1 = "R"
+letter2 = "U"
+letter3 = "I"
+letter4 = "H"
+letter5 = "A"
+letter6 = "N"
+letterX = "R"
+print(letterX)
+print(letter1)
+letterX = "X"
+print(letterX)
+print(letter1)
+print(letter2)
+print(letter3)
+print(letter4)
+print(letter5)
+print(letter6)
+
+letterX=letter1
+print(letterX)
+print(letter1)
+letter1="z"
+print(letterX)
+print(letter1)
