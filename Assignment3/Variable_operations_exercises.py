3a1589e809979a4cee76a64a36b00b48b1e2e1e0
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-10-02 00:29:13 -0600

    Update Variable_operations_exercises.py

--- Assignment3/Variable_operations_exercises.py
+++ Assignment3/Variable_operations_exercises.py
@@ -1,1 +1,11 @@
+##variable operation exercises
+sub_code="sub"
+subnr_int=2
+subnr_str="2"
+#print(sub_code+subnr_int) ##TypeError:can only concatenate str (not "int") to str
+print(sub_code+subnr_str)
 
+print(sub_code+" "+subnr_str)
+print(sub_code+" "+(subnr_str*3))
+print((sub_code+subnr_str)*3)
+print((sub_code*3)+""+(subnr_str*3))
