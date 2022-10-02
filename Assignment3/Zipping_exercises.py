0ef090f2771c6b1ae53898a64e484b91771c8725
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-10-02 01:55:23 -0600

    Create Zipping_exercises.py

---
+++ Assignment3/Zipping_exercises.py
@@ -0,0 +1,28 @@
+#Zipping exercises
+first_order=[]
+second_order=[]
+
+faces=['face1.png','face2.png','face3.png','face4.png','face5.png']*5
+houses=['house1.png','house2.png','house3.png','house4.png','house5.png']*5
+
+first_order.extend(faces)
+first_order.extend(houses)
+first_order.extend(faces)
+first_order.extend(houses)
+
+
+face_s=['face1.png','face2.png','face3.png','face4.png','face5.png']*5
+house_s=['house1.png','house2.png','house3.png','house4.png','house5.png']*5
+
+second_order.extend(house_s)
+second_order.extend(face_s)
+second_order.extend(house_s)
+second_order.extend(face_s)
+
+post_cues=['cue1']*50+['cue2']*50
+
+face_house = list(zip(first_order,second_order,post_cues))
+
+np.random.shuffle(face_house)
+print(face_house)
+print(len(face_house))
