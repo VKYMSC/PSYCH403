a22f27f93fe55cd5638f3c9cbae01f126e811b47
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-10-02 02:02:42 -0600

    Create Slicing_exercises.py

---
+++ Assignment3/Slicing_exercises.py
@@ -0,0 +1,10 @@
+#Slicing exercises
+list100=list(range(0,101))
+print(list100[:])
+print(list100[:10]) 
+print(list100[1::2]) 
+temp=list100[-4:]   
+print(temp[::-1])
+print(list100[40:44] == list100[39:43])
+print(list100[40:44] < list100[39:43])
+print(list100[40:44] > list100[39:43]) 
