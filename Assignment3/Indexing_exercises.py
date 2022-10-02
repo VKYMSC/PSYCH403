4f78be226fc1c12790f2dba265c6407f8d360552
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-10-02 01:59:08 -0600

    Create Indexing_exercises.py

---
+++ Assignment3/Indexing_exercises.py
@@ -0,0 +1,9 @@
+#Indexing exercises
+colors=["red", "orange", "yellow", "green", "blue", "purple"]
+print(colors[4])
+print(colors[4][2])
+print(colors[4][3])
+colors.remove("purple")
+colors.insert(5,"indigo")
+colors.append("violet")
+print(colors)
