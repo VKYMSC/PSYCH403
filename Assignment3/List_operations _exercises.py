7a830d995b949916cea27a7de2a2e59fc3d94580
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-10-02 00:52:50 -0600

    Create List_operations _exercises.py

---
+++ Assignment3/List_operations _exercises.py
@@ -0,0 +1,16 @@
+#List operations exercises
+numlist=[1,2,3]
+print(numlist*2) #print list twice
+
+numarr=np.array([1,2,3])
+print(numarr*2)
+#each object in the array is multiple by 2
+
+strlist=['do','re','mi','fa']
+print([strlist[0]*2+"",strlist[1]*2+"",strlist[2]*2+"",strlist[3]*2+""])
+print(strlist*2)
+print([strlist[0],strlist[0],strlist[1],strlist[1],
+    strlist[2],strlist[2],strlist[3],strlist[3]])
+
+print([[strlist[0],strlist[0]],[strlist[1],strlist[1]],
+    [strlist[2],strlist[2]],[strlist[3],strlist[3]]])
