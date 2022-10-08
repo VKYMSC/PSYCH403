fee1188fc0bec0a560ea5855f5e8b122ec9ba2ca
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-10-07 22:09:57 -0600

    Create Conditional_exercises.py

---
+++ Assignment4/Conditional_exercises.py
@@ -0,0 +1,58 @@
+
+##Conditional exercises
+## Condition 1
+response = "1"
+
+if response =='1' or response == '2':
+    print("OK")  
+    if response =='1':
+        print("Correct!")
+    elif response =='2':
+        print("Incorrect!")
+elif response == 'NaN':
+    print("subject did not respond")
+else:
+    print("subject pressed the wrong key")
+
+
+##Condition 2
+response = "2"
+
+if response =='1' or response == '2':
+    print("OK")  
+    if response =='1':
+        print("Correct!")
+    elif response =='2':
+        print("Incorrect!")
+elif response == 'NaN':
+    print("subject did not respond")
+else:
+    print("subject pressed the wrong key")
+    
+##Condition 3
+response = "NaN"
+
+if response =='1' or response == '2':
+    print("OK")  
+    if response =='1':
+        print("Correct!")
+    elif response =='2':
+        print("Incorrect!")
+elif response == 'NaN':
+    print("subject did not respond")
+else:
+    print("subject pressed the wrong key")
+
+##Condition 4
+response = "sdjada" #print anything else
+
+if response =='1' or response == '2':
+    print("OK")  
+    if response =='1':
+        print("Correct!")
+    elif response =='2':
+        print("Incorrect!")
+elif response == 'NaN':
+    print("subject did not respond")
+else:
+    print("subject pressed the wrong key")
