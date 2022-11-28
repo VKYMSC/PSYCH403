ef5429e297535e43d43f7c166ac86996e8498271
VKYMSC <43455460+VKYMSC@users.noreply.github.com>
2022-11-28 11:38:19 -0700

    Update Read_JSON.py

--- Assignment8/Read_JSON.py
+++ Assignment8/Read_JSON.py
@@ -1,19 +1,14 @@
-from psychopy import core, event, visual, monitors
+from psychopy import event, visual, monitors
+from psychopy import core
 import numpy as np
-import csv
+import csv 
 import os
-import json as json
 import pandas as pd
-
 
 mon = monitors.Monitor('myMonitor', width=38.1, distance=60)
 mon.setSizePix([1440,900])
 win = visual.Window(monitor=mon)
 
-filename = 'savejson_Assignment8'
-main_dir = os.getcwd()
-data_dir = os.path.join(main_dir,filename)
-#
 #blocks, trials, stims, and clocks
 nBlocks=2
 nTrials=4
@@ -27,17 +22,28 @@ sub_acc = [[-1]*nTrials]*nBlocks
 prob = [[-1]*nTrials]*nBlocks
 corr_resp = [[-1]*nTrials]*nBlocks
 resp_time = [[-1]*nTrials]*nBlocks
+
 blocks = [[0, 0, 0, 0], [1, 1, 1, 1]] 
 trials = [[0, 1, 2, 3], [0, 1, 2, 3]] 
-
-
 
 #create problems and solutions to show
 math_problems = ['1+3=','1+1=','3-2=','4-1='] #write a list of simple arithmetic
 solutions = [4,2,1,3] #write solutions
 prob_sol = list(zip(math_problems,solutions))
 
+sub_resp = dict()
+sub_acc = dict()
+prob = dict()
+corr_resp = dict()
+resp_time = dict()
+
 for block in range(nBlocks):
+    sub_resp[block] = [-1]*nTrials
+    sub_acc[block]= [-1]*nTrials
+    prob[block] = [-1]*nTrials
+    corr_resp[block] = [-1]*nTrials
+    resp_time[block] = [-1]*nTrials
+    
     for trial in range(nTrials):
         #what problem will be shown and what is the correct response?
         prob[block][trial] = prob_sol[np.random.choice(4)]
@@ -73,40 +79,49 @@ for block in range(nBlocks):
             sub_acc[block][trial] = 1 #arbitrary number for accurate response
         #incorrect- remember keys are saved as strings              
         elif sub_resp[block][trial] != str(corr_resp[block][trial]):
-            sub_acc[block][trial] = 0 #arbitrary number for inaccurate response 
-                                    #(should be something other than -1 to distinguish 
+            sub_acc[block][trial] = 2 #arbitrary number for inaccurate response 
+                                    #(should be something other than 0 to distinguish 
                                     #from non-responses)
                     
         #print results
         print('problem=', prob[block][trial], 'correct response=', 
               corr_resp[block][trial], 'subject response=',sub_resp[block][trial], 
-              'subject accuracy=',sub_acc[block][trial], 'reaction time=',
-              resp_time[block][trial])
+              'subject accuracy=',sub_acc[block][trial], 'reaction time=', resp_time[block][trial])
 
-win.close()
+print(sub_resp)
+print(sub_acc)
+print(prob)
+print(corr_resp)
+print(resp_time)
 
+filename = 'Assignment8.csv' 
+main_dir = os.getcwd()
+data_dir = os.path.join(main_dir,filename)
+#to save each data type individually with one block per row
 data_as_list = [prob, corr_resp, sub_resp, sub_acc, resp_time]
 
-with open(data_dir, 'w') as sub_data:
+with open(filename, 'w', newline='') as csvfile:
     fieldnames = ['block', 'trial', 'problem','corr_resp','sub_resp','sub_acc', 'resp_time']
-    data_writer = csv.DictWriter(sub_data, fieldnames=fieldnames)
-    data_writer.writeheader()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+    writer.writeheader()
     for block in range(nBlocks):
         data_as_dict = []
         for a,b,c,d,e,f,g in zip(blocks[block], trials[block], prob[block], corr_resp[block], sub_resp[block], sub_acc[block], resp_time[block]):
-            data_as_dict.append({'block':a, 'trial':b, 'problem':c,'corr_resp':d,'sub_resp':e,'sub_acc':f, 'resp_time':g})
+             data_as_dict.append({'block':a, 'trial':b, 'problem':c,'corr_resp':d,'sub_resp':e,'sub_acc':f, 'resp_time':g})
         #print(data_as_dict)
         for iTrial in range(nTrials):
-            data_writer.writerow(data_as_dict[iTrial])
+            writer.writerow(data_as_dict[iTrial])
 
-df = pd.read_csv('savejson_Assignment8')
+df = pd.read_csv('Assignment8.csv')
 print(df)
 
-acc_trials = df.loc[df['sub_acc'] == 1] 
+acc_trials = df.loc[df['sub_acc'] == 2] 
 print(acc_trials)
 print(len(acc_trials)/len(df['sub_acc']))
 
-without_respones = df.loc[df['sub_resp'] == 0] 
+without_respones = df.loc[df['sub_resp'] == -1] 
 print(without_respones)
 print(len(without_respones)/len(df['sub_resp']))
+
+win.close()
